5ad8fa6876bdd8245850e3dbb8375759c42fd804
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-14 13:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	let ans =[];
+	let finall=0;
+	while(finall<str.length){
+		ans.push(str.slice(finall,Math.min(finall+size,str.length)));
+		final+=size;
+	}
+	return ans;
 }
 
 // Do not change the code below
